b43f2e37d51aec71213ea9f86270cde297714bd1
Daniel Asay <danielasay@fas.harvard.edu>
2024-11-13 10:23:45 -0500

    cut down on code

--- scanbuddy/proc/__init__.py
+++ scanbuddy/proc/__init__.py
@@ -44,13 +44,7 @@ class Processor:
 
         num_vols = ds[(0x0020, 0x0105)].value
 
-        #if self._key == 1:
-
-            #self._plot_dict = dict.fromkeys(range(1, num_vols+1), None)
-
         data_path = os.path.dirname(os.path.dirname(os.path.dirname(path)))
-
-        #logger.info(f'data path {data_path}')
 
         num_dicoms = len([name for name in os.listdir(data_path) if name.endswith('.dcm')])
 
@@ -59,23 +53,6 @@ class Processor:
         else:
             self._snr_interval = 5
 
-#        interval_mapping = {
-#            0: 2,
-#            1: 3,
-#            2: 4,
-#            3: 5,
-#            4: 6,
-#            5: 7,
-#            6: 8
-#        }  
-#            if self._key == 1:
-#                self._snr_interval = 1
-#            else:
-#                self._snr_interval = self.get_snr_interval(num_dicoms)
-        #if num_dicoms > 1:
-            #self._snr_interval     
-
-        #self._snr_interval = interval_mapping.get(num_dicoms, 9)
 
         logger.info(f'CURRENT SNR INTERVAL {self._snr_interval}')
 
@@ -84,10 +61,8 @@ class Processor:
 
         #self._plot_dict[self._key] = psutil.cpu_percent()
 
-        #pdb.set_trace()
-
         tasks = self.check_volreg(self._key)
-        ### edits need to be made here
+
         snr_tasks = self.check_snr(self._key)
         logger.debug('publishing message to volreg topic with the following tasks')
         logger.debug(json.dumps(tasks, indent=2))
@@ -107,7 +82,6 @@ class Processor:
             self._mask_threshold = self.get_mask_threshold(ds)
             x, y, z, _ = self._slice_means[self._key]['slice_means'].shape
 
-            #self._fdata_array = np.zeros((x, y, z, num_vols))
             self._fdata_array = np.empty((x, y, z, num_vols))
             self._numpy_4d_mask = np.zeros(self._fdata_array.shape, dtype=bool)
 
@@ -115,26 +89,17 @@ class Processor:
             logger.info(f'shape of first slice means: {self._slice_means[self._key]['slice_means'].shape}')
 
         if self._key >= 5:
-            #pdb.set_trace()
             insert_position = self._key - 5
-            #self._fdata_array[:, :, :, insert_position] = self._slice_means[self._key]['slice_means']
             self._fdata_array[:, :, :, insert_position] = self._slice_means[self._key]['slice_means'].squeeze()
 
-        #elif self._key > 5:
-            #self._fdata_array = np.concatenate((self._fdata_array, self._slice_means[self._key]['slice_means']), axis=3)
         if self._key > 53 and (self._key % self._snr_interval == 0) and self._key < num_vols:
-            #logger.info(f'shape of slice_means_array: {self._slice_means_array.shape}')
             logger.info(f'shape of fdata_array: {self._fdata_array.shape}')
             logger.info('publishing message to plot_snr topic')
 
             snr_thread = threading.Thread(target=self.calculate_and_publish_snr)
             snr_thread.start()
-            #snr_metric = round(self.calc_snr(), 2)
-            #logger.info(f'running snr metric: {snr_metric}')
-            #pub.sendMessage('plot_snr', snr_metric=snr_metric)
 
         if self._key == num_vols:
-            #snr_thread.stop()
             logger.debug('RUNNING FINAL SNR CALCULATION')
             snr_metric = round(self.calc_snr(), 2)
             logger.info(f'running snr metric: {snr_metric}')
@@ -235,7 +200,6 @@ class Processor:
 
             total_voxel_count += slice_voxel_count
 
-        #return slice_weighted_mean_mean / total_voxel_count
         return slice_weighted_snr_mean / total_voxel_count
 
 
@@ -308,96 +272,6 @@ class Processor:
         return seq[(0x0018, 0x1250)].value
 
 
-        #pdb.set_trace()
-
-        '''
-
-        if self._slice_means_array.shape[0] != self._mask_array.shape[2]:
-            raise ValueError("The number of slices in mean_voxel_intensities must equal the number of slices in binary_mask.")
-
-
-        slice_weighted_mean_mean = 0.0
-        slice_weighted_stdev_mean = 0.0
-        total_voxel_count = 0
-
-        for slice_idx in range(self._slice_means_array.shape[0]):
-            slice_voxel_count = np.sum(self._mask_array[:,:,slice_idx] == False)
-            slice_data = self._slice_means_array[slice_idx, :]
-
-            if slice_voxel_count > 0:
-                slice_mean = np.mean(slice_data)
-                slice_stdev = np.std(slice_data, ddof=1)
-
-                if np.isfinite(slice_mean) and np.isfinite(slice_stdev):
-                    slice_weighted_mean_mean += (slice_mean * slice_voxel_count)
-                    slice_weighted_stdev_mean += (slice_stdev * slice_voxel_count)
-                    total_voxel_count += slice_voxel_count
-
-        if total_voxel_count == 0:
-            raise ValueError("Total voxel count is zero. Check your binary mask.")  
-
-        slice_weighted_mean = slice_weighted_mean_mean / total_voxel_count
-        slice_weighted_stdev = slice_weighted_stdev_mean / total_voxel_count    
-
-        slice_weighted_snr = slice_weighted_mean / slice_weighted_stdev if slice_weighted_stdev != 0 else 0 
-
-        return slice_weighted_snr
-        '''
-
-        '''
-        slice_wmean_sum = 0
-        slice_wstd_sum = 0
-        slice_wsnr_sum = 0
-        total_masked_voxels = 0
-        total_voxel_count = 0
-        slice_summary = []
-
-        slice_voxel_counts = self.get_slice_voxel_count()
-
-        for slice_idx in range(self._slice_means_array.shape[0]):
-
-            volume_means = self._slice_means_array[slice_idx, :]
-            # compute the raw slice mean across time
-            mean = volume_means.mean()
-            # compute the raw slice std across time
-            std = volume_means.std(ddof=1)
-            if std == 0:
-                snr = 0
-            else:
-                snr = mean / std
-
-            # store raw statistics within the slice summary
-            #slice_summary.append(SliceRow(i + 1, mean, std, snr))
-            # count the number of un-masked voxels for the i'th slice
-
-            #num_masked_voxels = (self._mask_array[:,:,slice_idx] == True).sum()
-
-            slice_voxel_count = slice_voxel_counts[slice_idx]
-
-            slice_wsnr_sum += snr * slice_voxel_count
-
-            total_voxel_count += slice_voxel_count
-
-            # keep a running sum of the number of un-masked voxels
-
-            #total_masked_voxels += num_masked_voxels
-
-            # keep running sums of the weighted mean, std, and snr
-            #slice_wmean_sum += mean * num_masked_voxels
-            #slice_wstd_sum += std * num_masked_voxels
-
-            #slice_wsnr_sum += snr * num_masked_voxels
-
-        # compute the weighted slice-based mean, standard deviation, and snr
-        #wm = slice_wmean_sum / total_masked_voxels
-        #ws = slice_wstd_sum / total_masked_voxels
-        wsnr = slice_wsnr_sum / total_voxel_count
-
-        logger.debug(wsnr)
-        
-        return wsnr
-        '''
-
     def get_slice_voxel_count(self, lower_threshold_to_zero=None):
         """
         Compute the voxel counts per slice based on a masking array and optional thresholding.
@@ -418,8 +292,6 @@ class Processor:
 
         # Calculate voxels for each slice in the z-dimension
         for slice_idx in range(num_slices):
-            #pdb.set_trace()
-            #masked_data = np.ma.masked_array(self._slice_means_array[slice_idx,:], mask=self._mask_array[:,:,slice_idx])
             # Counting unmasked (valid) voxels in each slice
             slice_voxel_counts[slice_idx] = np.sum(~self._mask_array[slice_idx])    
 
@@ -440,30 +312,4 @@ class Processor:
 
         return tasks
 
-        ## get the index thats snr_interval (10, for example) to the left of the current
-        '''
-        try:
-            left_index = current_idx - int(self._snr_interval)
-            for idx in range(left_index, current_idx):
-                value = self._instances.values()[idx]
-                logger.debug(f'adding dicom {value["path"]} to snr calculation task')
-                tasks.append(value)
-        except IndexError:
-            pass
-
-        return tasks
-
-        # extract the latest snr value calculation
-        
-        if len(self._instances) == self._snr_interval:
-            prev_snr = 0.0
-        else:
-            for self._key in reversed(self._instances.self._keys()):
-                if self._instances[self._key]['snr'] is None:
-                    continue
-                else:
-                    prev_snr = float(self._instances[self._key]['snr'])
-                    logger.info(f'found previously calculated snr at index {self._key}')
-                    return tasks, prev_snr
-        '''
 
